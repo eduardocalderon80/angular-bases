3b8d9506ee05b9217dec20a12a574138ad468ddf
eduardocalderon80 <54388243+eduardocalderon80@users.noreply.github.com>
2024-12-03 00:25:38 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter-component';
 import { heroesModule } from './heroes/heroes-module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { heroesModule } from './heroes/heroes-module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    heroesModule
+    heroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
